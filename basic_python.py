ab60ab2108232d3c6bc39bc121a53f0254aa2d60
Vasundara4 <vasundaragowda13494@gmail.com>
2021-02-02 23:07:46 -0500

    First changes in the repo

---
+++ basic_python.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#Basic Python program
+num=5
+mul=1
+
+for i in range(0,11):
+    print(str(num)+ "X" +str(i) + "=" + str(num*i))
